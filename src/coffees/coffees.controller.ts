ddf2dbf96cf6c8f88ab8abbc9985130622344fbd
Diego Vicuna <diego.vicuna@funiber.org>
2023-11-16 10:18:40 -0500

    feature: building blocks

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -14,6 +14,7 @@ import { UpdateCoffeeDto } from './dto/update-coffee.dto';
 import { PaginationQueryDto } from 'src/common/dto/pagination-query.dto/pagination-query.dto';
 import { Public } from '../common/decorators/public.decorator';
 import {ParseIntPipe} from "../common/pipes/parse-int/parse-int.pipe";
+import {Protocol} from "../common/decorators/protocol.decorator";
 
 @Controller('coffees')
 export class CoffeesController {
@@ -21,7 +22,8 @@ export class CoffeesController {
 
     @Public()
     @Get()
-    async findAll(@Query() paginationQuery:PaginationQueryDto) {
+    async findAll(@Protocol('https') protocol:string, @Query() paginationQuery:PaginationQueryDto) {
+        console.log(protocol);
         await new Promise(resolve => setTimeout(resolve, 5000));
         return this.coffeesService.findAll(paginationQuery);
     }
